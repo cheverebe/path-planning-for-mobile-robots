8107185215d9f862a41a0f976a0a4d233d52f27f
cheverebe <alejandrorebecchi@gmail.com>
2015-07-24 17:13:31 -0300

    2 drones working

--- classes.py
+++ classes.py
@@ -6,160 +6,193 @@ from matplotlib import collections  as mc
 import matplotlib.pyplot as plt
 import pylab as pl
 
-def primitives():
-	return [(lambda pos: Position([pos.coordinates[0]+.01,pos.coordinates[1]])),
-			(lambda pos: Position([pos.coordinates[0]-.01,pos.coordinates[1]])),
-			(lambda pos: Position([pos.coordinates[0],pos.coordinates[1]+.01])),
-			(lambda pos: Position([pos.coordinates[0],pos.coordinates[1]-.01])),
-			(lambda pos: Position([pos.coordinates[0]+.01,pos.coordinates[1]+.01])),
-			(lambda pos: Position([pos.coordinates[0]-.01,pos.coordinates[1]-.01])),
-			(lambda pos: Position([pos.coordinates[0]-.01,pos.coordinates[1]+.01])),
-			(lambda pos: Position([pos.coordinates[0]+.01,pos.coordinates[1]-.01]))]
+distance_factor = .03
 
+def primitives():
+    return [(lambda pos: Position([pos.coordinates[0] + distance_factor, pos.coordinates[1]])),
+            (lambda pos: Position([pos.coordinates[0] - distance_factor, pos.coordinates[1]])),
+            (lambda pos: Position([pos.coordinates[0], pos.coordinates[1] + distance_factor])),
+            (lambda pos: Position([pos.coordinates[0], pos.coordinates[1] - distance_factor])),
+            (lambda pos: Position([pos.coordinates[0] + distance_factor, pos.coordinates[1] + distance_factor])),
+            (lambda pos: Position([pos.coordinates[0] - distance_factor, pos.coordinates[1] - distance_factor])),
+            (lambda pos: Position([pos.coordinates[0] - distance_factor, pos.coordinates[1] + distance_factor])),
+            (lambda pos: Position([pos.coordinates[0] + distance_factor, pos.coordinates[1] - distance_factor]))]
+
+
+class Target(object):
+    def __init__(self, center, radius):
+        c = center.as_tuple()
+        self.lx = c[0]-radius
+        self.hx = c[0]+radius
+        self.ly = c[1]-radius
+        self.hy = c[1]+radius
+        
+    def check(self, pos):        
+        return self.lx <= pos.as_tuple()[0] and \
+                pos.as_tuple()[0] <= self.hy and \
+                self.hy >= pos.as_tuple()[1] and \
+                pos.as_tuple()[1] >= self.ly
+        
+    def plot(self, ax):
+        target_lines = [((self.lx, self.hy), (self.lx, self.ly)), ((self.lx, self.hy), (self.hx, self.hy)), ((self.hx, self.hy), (self.hx, self.ly)),
+                        ((self.lx, self.ly), (self.hx, self.ly))]
+        for line in target_lines:
+            n_pos = line[0]
+            p_pos = line[1]
+
+            DATA = (n_pos, p_pos)
+            (x, y) = zip(*DATA)
+            color = 'y'
+            ax.plot(x, y, marker='o', color=color)
 
 class RRT(object):
-	def __init__(self, initial_state, target_checker, primitives):
-		self.initialState = initial_state
-		self.droneCount = initial_state.count()
-		self.states = [initial_state]
-		self.target_checker = target_checker
-		self.primitives = primitives
-		self.random_points = []
+    def __init__(self, initial_state, target, primitives):
+        self.initialState = initial_state
+        self.droneCount = initial_state.count()
+        self.states = [initial_state]
+        self.target = target
+        self.primitives = primitives
+        self.random_points = []
 
-	def statesInTarget(self):
-		statesRes = []
-		for state in self.states:
-			#print(state)
-			in_target = True
-			#print(state.positions)	
-			for position in state.positions:
-				in_target = in_target and self.target_checker(position)
-			#print(in_target)
-			if in_target:
-				statesRes.append(state)
-		return statesRes
+    def statesInTarget(self):
+        statesRes = []
+        for state in self.states:
+            # print(state)
+            in_target = True
+            # print(state.positions)
+            for position in state.positions:
+                in_target = in_target and self.target.check(position)
+            # print(in_target)
+            if in_target:
+                statesRes.append(state)
+        return statesRes
 
+    def grow(self, max_iter):
+        for iteration in range(max_iter):
+            q_rand = self.sample_free()
+            q_nearest = self.nearest(q_rand)
+            q_new = self.steer(q_nearest, q_rand)
+            if self.collisionFree(q_nearest, q_new) and self.formationMaintained(q_new):
+                for point in q_rand:
+                    self.random_points.append(point)
+                self.states.append(q_new)
 
-	def grow(self, max_iter):
-		for iteration in range(max_iter):
-			q_rand = self.sample_free()
-			q_nearest = self.nearest(q_rand)
-			q_new = self.steer(q_nearest, q_rand)
-			if self.collisionFree(q_nearest,q_new):
-				for point in q_rand:
-					self.random_points.append(point)
-				self.states.append(q_new)
+    def collisionFree(self, q_nearest, q_new):
+        return True
 
-	def collisionFree(self, q_nearest,q_new):
-		return True
+    def sample_free(self):
+        sample = [[random.randint(-200, 200) / 100.0, -random.randint(-200, 200) / 100.0] for i in range(self.droneCount)]
+        return [Position(pos) for pos in sample]
 
-	def sample_free(self):
-		sample = [[random.randint(0,150)/100.0,-random.randint(0,150)/100.0] for i in range(self.droneCount)]
-		return [Position(pos) for pos in sample]
+    def nearest(self, q_rand):
+        # default
+        nearest = self.states[0]
+        min_dist = 0
+        for i in range(self.droneCount):
+            position = nearest.positions[i]
+            rand_position = q_rand[0]
+            min_dist += position.distance(rand_position)
+        # loop
+        for state in self.states:
+            total_dist = 0
+            for i in range(self.droneCount):
+                position = state.positions[i]
+                rand_position = q_rand[i]
+                total_dist += position.distance(rand_position)
+            if total_dist < min_dist:
+                min_dist = total_dist
+                nearest = state
+        return nearest
 
-	def nearest(self, q_rand):
-		#default
-		nearest = self.states[0]
-		min_dist = 0
-		for i in range(self.droneCount):
-			position = nearest.positions[i]
-			rand_position = q_rand[0]
-			min_dist += position.distance(rand_position)
-		#loop
-		for state in self.states:
-			total_dist = 0
-			for i in range(self.droneCount):
-				position = state.positions[i]
-				rand_position = q_rand[i]
-				total_dist += position.distance(rand_position)
-			if total_dist < min_dist:
-				min_dist = total_dist
-				nearest = state
-		return nearest
+    def steer(self, q_nearest, q_rand):
+        new_positions = []
+        for i in range(self.droneCount):
+            position = q_nearest.positions[i]
+            best_pos = self.primitives[0](position)
+            best_dist = position.distance(q_rand[i])
+            for primitive in self.primitives:
+                new_pos = primitive(position)
+                new_dist = new_pos.distance(q_rand[i])
+                if new_dist < best_dist:
+                    best_dist = new_dist
+                    best_pos = new_pos
+            new_positions.append(best_pos)
+        return State(new_positions, q_nearest)
 
-	def steer(self, q_nearest, q_rand):
-		new_positions = []
-		for i in range(self.droneCount):
-			position = q_nearest.positions[i]
-			best_pos = self.primitives[0](position)
-			best_dist = position.distance(q_rand[i])
-			for primitive in self.primitives:
-				new_pos = primitive(position)
-				new_dist = new_pos.distance(q_rand[i])
-				if new_dist < best_dist:
-					best_dist = new_dist
-					best_pos = new_pos
-			new_positions.append(best_pos)
-		return State(new_positions, q_nearest)
+    def plot(self):
+        color_samples = ['r', 'g', 'b', 'c', 'm', 'y']
+        lines = []
+        colors = []
+        pointsx = []
+        pointsy = []
 
-	def plot(self):
-		color_samples = ['r', 'g', 'b', 'c', 'm', 'y']
-		lines = []
-		colors = []
-		pointsx = []
-		pointsy = []
+        fig, ax = plt.subplots()
 
-		fig, ax = plt.subplots()
+        sit = self.statesInTarget()
+        for state in self.states:
+            parent = state.parent
+            if parent:
+                for i in range(self.droneCount):
+                    n_pos = state.positions[i].as_tuple()
+                    p_pos = parent.positions[i].as_tuple()
 
-		sit = self.statesInTarget()
-		for state in self.states:
-			parent = state.parent
-			if parent:
-				for i in range(self.droneCount):
-					n_pos = state.positions[i].as_tuple()
-					p_pos = parent.positions[i].as_tuple()
+                    lines.append([n_pos, p_pos])
+                    DATA = (n_pos, p_pos)
+                    (x, y) = zip(*DATA)
+                    color = 'm' if state in sit else color_samples[i % len(color_samples)]
+                    ax.plot(x, y, marker='o', color=color)
 
-					lines.append([n_pos, p_pos])
-					DATA = (n_pos, p_pos)
-					(x,y) = zip(*DATA)
-					color = 'm' if state in sit else color_samples[i%len(color_samples)]
-					ax.plot(x, y, marker='o', color=color)
+        random_points_X = [point.as_tuple()[0] for point in self.random_points]
+        random_points_Y = [point.as_tuple()[1] for point in self.random_points]
+        random_points_color = ['k'] * len(random_points_X)
+        ax.scatter(random_points_X, random_points_Y, c=random_points_color)
+        self.target.plot(ax)
+        plt.show()
 
-		random_points_X = [ point.as_tuple()[0] for point in self.random_points]
-		random_points_Y = [ point.as_tuple()[1] for point in self.random_points]
-		random_points_color = ['k'] * len(random_points_X)
-		ax.scatter(random_points_X, random_points_Y, c=random_points_color)
+    def get_solution(self):
+        solution = []
+        sit = self.statesInTarget()
+        if len(sit)>0:
+            state = sit[0]
+            while state:
+                solution.append(state)
+                state = state.parent
+            solution = reversed(solution)
+        return solution
 
-		c = np.array(colors)
+    def formationMaintained(self, q_new):
+        distance_threshhold = .3
 
-		target_points_X = [ ]
-		target_points_Y = [ ]
-		
-		target_lines= [((0.5,-0.5),(0.5,-0.9)),((0.5,-0.5),(0.9,-0.5)),((0.9,-0.5),(0.9,-0.9)),((0.5,-0.9),(0.9,-0.9))]
-		for line in target_lines:
-			n_pos = line[0]
-			p_pos = line[1]
+        pos_leader = q_new.positions[0]
+        for position in q_new.positions:
+            if pos_leader.distance(position) > distance_threshhold:
+                return False
+        return True
 
-			DATA = (n_pos, p_pos)
-			(x,y) = zip(*DATA)
-			color = 'y'
-			ax.plot(x, y, marker='o', color=color)
-
-		plt.show()
 
 class State(object):
-	def __init__(self, positions, parent=None):
-		self.positions = positions
-		self.parent = parent
+    def __init__(self, positions, parent=None):
+        self.positions = positions
+        self.parent = parent
 
-	def count(self):
-		return len(self.positions)
+    def count(self):
+        return len(self.positions)
 
-	def __str__(self):
-		return ', '.join([position.__str__() for position in self.positions])
+    def __str__(self):
+        return ', '.join([position.__str__() for position in self.positions])
 
 
 class Position(object):
-	def __init__(self, coordinates):
-		self.coordinates = coordinates
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
 
-	def as_tuple(self):
-		return (self.coordinates[0], self.coordinates[1])
+    def as_tuple(self):
+        return (self.coordinates[0], self.coordinates[1])
 
-	def distance(self,p2):
-		a = np.array(self.as_tuple())
-		b = np.array(p2.as_tuple())
-		return np.linalg.norm(a-b)
+    def distance(self, p2):
+        a = np.array(self.as_tuple())
+        b = np.array(p2.as_tuple())
+        return np.linalg.norm(a - b)
 
-	def __str__(self):
-		return str(self.coordinates)
+    def __str__(self):
+        return str(self.coordinates)
--- vrep-test.py
+++ vrep-test.py
@@ -12,52 +12,75 @@ import math
 from classes import *
 
 # just in case, close all opened connections
-vrep.simxFinish( -1 )
+vrep.simxFinish(-1)
 
-PORT=19997
+PORT = 19997
 clientID = vrep.simxStart("127.0.0.1", PORT, True, True, 5000, 5)
 random.seed()
 
+
+def run(path, handles):
+    for state in path:
+        for i in range(len(handles)):
+            handle = handles[i]
+            ret, pos = vrep.simxGetObjectPosition(clientID, handle, -1, vrep.simx_opmode_oneshot_wait)
+
+            # wait until it's almost in the position
+            # ------
+
+            if (not ret == vrep.simx_return_ok):
+                print('Failed to retrieve position for Quadricopter_target')
+                sys.exit(1)
+
+            #print pos
+            new_pos = pos
+            new_pos[0] = state.positions[i].as_tuple()[0]
+            new_pos[1] = state.positions[i].as_tuple()[1]
+
+            ret = vrep.simxSetObjectPosition(clientID, handle, -1, new_pos, vrep.simx_opmode_oneshot_wait)
+            if (not ret == vrep.simx_return_ok):
+                print('Failed to set position for Quadricopter_target')
+                sys.exit(1)
+
+
 # check if client connection successful
 if clientID == -1:
-  print('Could not connect to remote API server')
-  sys.exit( 1 )
+    print('Could not connect to remote API server')
+    sys.exit(1)
 
 print('Connected to remote API server')
 
-ret, handle = vrep.simxGetObjectHandle( clientID, 'Quadricopter_target', vrep.simx_opmode_oneshot_wait )
-handles = [handle]
-if ( not ret == vrep.simx_return_ok ):
-  print('Failed to retrieve handle for Quadricopter_target')
-  sys.exit( 1 )
-ret, pos = vrep.simxGetObjectPosition( clientID, handle, -1, vrep.simx_opmode_oneshot_wait )
-init_pos = Position([pos[0],pos[1]])
-initial_state = State([init_pos])
-target_checker = lambda pos: (0.5<=pos.as_tuple()[0] and pos.as_tuple()[0]<=0.9 and -0.5>=pos.as_tuple()[1] and pos.as_tuple()[1]>=-0.9)
-rrt = RRT(initial_state, target_checker, primitives())
-rrt.grow(1000)
+#----------------FINDING QUADS
+ret, handle = vrep.simxGetObjectHandle(clientID, 'Quadricopter_target', vrep.simx_opmode_oneshot_wait)
+if (not ret == vrep.simx_return_ok):
+    print('Failed to retrieve handle for Quadricopter_target')
+    sys.exit(1)
+handles = []
+drones = -1
+initial_positions = []
+while ret == vrep.simx_return_ok:
+    handles.append(handle)
+    ret, pos = vrep.simxGetObjectPosition(clientID, handle, -1, vrep.simx_opmode_oneshot_wait)
+    init_pos = Position([pos[0], pos[1]])
+    initial_positions.append(init_pos)
+
+    drones += 1
+    qid = "Quadricopter_target#%d" % drones
+    ret, handle = vrep.simxGetObjectHandle(clientID, qid, vrep.simx_opmode_oneshot_wait)
+print(handles)
+initial_state = State(initial_positions)
+
+#-----------------FINDING TARGET
+ret, handle = vrep.simxGetObjectHandle(clientID, 'ResizableFloor_1_5', vrep.simx_opmode_oneshot_wait)
+if (not ret == vrep.simx_return_ok):
+    print('Failed to retrieve handle for ResizableFloor_1_5')
+    sys.exit(1)
+ret, pos = vrep.simxGetObjectPosition(clientID, handle, -1, vrep.simx_opmode_oneshot_wait)
+target = Target(Position([pos[0], pos[1]]), .2)
+
+#---------------CREATING TREE
+rrt = RRT(initial_state, target, primitives())
+rrt.grow(1200)
 rrt.plot()
-
-
-def run(path, handles):
-
-	for step in path:
-		for i in range(len(handles)):
-			handle = handles[i]
-			ret, pos = vrep.simxGetObjectPosition( clientID, handle, -1, vrep.simx_opmode_oneshot_wait )
-
-			# wait until it's almost in the position
-			#------
-
-			if ( not ret == vrep.simx_return_ok ):
-				print('Failed to retrieve position for Quadricopter_target')
-				sys.exit( 1 )
-
-			print pos
-			new_pos = step[i]
-
-			ret = vrep.simxSetObjectPosition( clientID, handle, -1, new_pos, vrep.simx_opmode_oneshot_wait )
-			if ( not ret == vrep.simx_return_ok ):
-				print('Failed to set position for Quadricopter_target')
-				sys.exit( 1 )
-
+path = rrt.get_solution()
+run(path, handles)
